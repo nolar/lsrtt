78b607053057719c1c515cba5cfc80997a1d4dc5
Sergey Vasilyev <nolar@nolar.info>
2017-12-05 19:43:53 +0100

    Fetch the dicts for the database records
    This will slow down the scripts a bit, but is more convenient
    for the code.

--- lsrtt/db/connection.py
+++ lsrtt/db/connection.py
@@ -9,4 +9,12 @@ DEFAULT_DB_PATH = 'db.sqlite'
 @contextlib.contextmanager
 def connect_db(db_path):
     with sqlite3.connect(db_path) as con:
+        con.row_factory = dict_factory
         yield con
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
